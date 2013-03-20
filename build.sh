5f96a6a2c119cbbdd5b5990dbf49e0c2fd85ac27
Frumple <frumple1@gmail.com>
2013-03-19 21:03:19 -0700

    First pass on adding route cards.

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@ mkdir Output
 cp -R Fonts Output
 cp -R Stylesheets Output
 
-ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/MountPleasant.yml > output/MountPleasant.html
-ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/Fairview.yml > output/Fairview.html
-ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/Marpole.yml > output/Marpole.html
-ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/Victoria-Fraserview.yml > output/Victoria-Fraserview.html
-ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/Renfrew-Collingwood.yml > output/Renfrew-Collingwood.html
-ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/Hastings-Sunrise.yml > output/Hastings-Sunrise.html
-ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/DowntownVancouver.yml > output/DowntownVancouver.html
-ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/PointGrey.yml > output/PointGrey.html
+ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/PassengerCard/MountPleasant.yml > output/MountPleasant.html
+ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/PassengerCard/Fairview.yml > output/Fairview.html
+ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/PassengerCard/Marpole.yml > output/Marpole.html
+ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/PassengerCard/Victoria-Fraserview.yml > output/Victoria-Fraserview.html
+ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/PassengerCard/Renfrew-Collingwood.yml > output/Renfrew-Collingwood.html
+ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/PassengerCard/Hastings-Sunrise.yml > output/Hastings-Sunrise.html
+ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/PassengerCard/DowntownVancouver.yml > output/DowntownVancouver.html
+ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/PassengerCard.css --template CardTemplates/PassengerCard.slim --data CardData/PassengerCard/PointGrey.yml > output/PointGrey.html
+
+ruby DeckGenerator/deck_generator.rb --stylesheet Stylesheets/RouteCard.css --template CardTemplates/RouteCard.slim --data CardData/RouteCard/Routes.yml > output/Routes.html
